21a0a3f7558cc683ef486b3d7287281523f75df0
matheus <102304929+matheuskkj77@users.noreply.github.com>
2023-05-12 14:18:48 -0300

    Add files via upload

---
+++ emoji generator/script.js
@@ -0,0 +1,10 @@
+const emoji = ["😀", "😃",  "😄",  "😁", "😅", "🤣", "😂", "🙂", "🙃", "🫠", "😉", "😊", "😇", "🥰", "😍", "🤩", "😘", "😗", "☺", "😚", "😙",
+"😛", "😜", "🤪", "😝", "🤑", "🤗", "🤭", "🤫", "🤔", "🤐", "🤨", "😐", "😑", "😶", "😶‍🌫️", "😏", "😒", "🙄", "😬", "😮‍💨", "🤥", "😌", "😔", "😪", "🤤", "😴",
+"😷", "🤒", "🤕", "🤢", "🤮", "🤧", "🥵", "🥶", "🥴", "😵", "😵‍💫", "🤯", "🤠", "🥳", "😎"]
+
+const generateBtn = document.getElementById("btn")
+document.getElementById("emoji-container").textContent= emoji[0]
+generateBtn.addEventListener('click', function (){
+    let output = emoji[Math.floor(Math.random() * emoji.length)]
+    document.getElementById("emoji-container").textContent = output
+})
